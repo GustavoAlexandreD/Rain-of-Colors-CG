555395f4fa9de8ee1f55c97fa776f5ca2aab9323
Gustavo Alexandre <gustavoalexandredg@gmail.com>
2026-03-17 13:35:09 -0300

    adicionando background da tela de jogo

--- src/game/front_end/TelasPrincipais/Jogo/Jogo.py
+++ src/game/front_end/TelasPrincipais/Jogo/Jogo.py
@@ -1,5 +1,6 @@
 import pygame
 
+from game.front_end.Componentes.Background import Background
 from game.front_end.TelasPrincipais.Jogo.Jogo_layout import JogoLayout
 from game.front_end.TelasPrincipais.Jogo.Jogo_controller import JogoController
 from game.front_end.Componentes.Text import draw_text_raster
@@ -12,6 +13,20 @@ class Jogo:
         self.width = width
         self.height = height
 
+        # ==================================================
+        # Background
+        # ==================================================
+
+        self.background = Background(
+            width,
+            height,
+            "assets/images/JogoBackground.jpeg"
+        )
+
+        self.background.render_once()
+
+
+
         self.layout = JogoLayout(width, height)
         self.controller = JogoController()
 
@@ -21,6 +36,8 @@ class Jogo:
         return self.controller.update(input_handler)
 
     def draw(self, surface):
+
+        self.background.draw(surface)
 
         pixel_array = pygame.PixelArray(surface)
 
