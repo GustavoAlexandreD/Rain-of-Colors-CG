c94d6e0a49179df7399477ceb4aa46d38a1924a3
Gustavo Alexandre <gustavoalexandredg@gmail.com>
2026-03-19 22:52:08 -0300

    fix: ajustando taxa de spawn dos objetos

--- src/game/scripts/ObjetosdaChuva/FactoryChuva.py
+++ src/game/scripts/ObjetosdaChuva/FactoryChuva.py
@@ -12,13 +12,13 @@ class FactoryChuva:
 
         rand = random.random()
 
-        if rand < 0.88:
+        if rand < 0.84:
             return Gota(x, y, 40, 20)
 
-        elif rand < 0.86:
+        elif rand < 0.88:
             return Estrela(x, y)
 
-        elif rand < 0.93:
+        elif rand < 0.94:
             return Gelo(x, y)
 
         else:
